d65e7ce5410446818791ac78d6c4d5efe15b7c7b
TheClouD-654 <arunangshucloud@gmail.com>
2026-04-17 00:09:47 +0530

    commit

--- data/products.js
+++ data/products.js
@@ -10,7 +10,7 @@ const PRODUCTS = {
     newPriceDisplay: '$1.99',
     displayAmount: 1.99,
     displayCurrency: 'USD',
-    chargeAmount: 0.011,
+    chargeAmount: 0.11,
     chargeCurrency: 'USD',
     zipName: 'LevelUp-Circle-Starter-Bundle.zip',
     zipUrlEnvKey: 'PRODUCT_STARTER_ZIP_URL'
